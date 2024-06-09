a01ce212838700c80021caaa5c13c8dd981bab14
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-06-09 10:59:40 -0500

    better practice

--- src/server/utils/database.js
+++ src/server/utils/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/database", {
+        await mongoose.connect("mongodb://localhost:27017/synk", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
